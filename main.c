078d86a17dcb77eef87012833081dd9d4b7db552
Ander Moldes Herrador <ander.moldes@opendeusto.es>
2021-04-09 16:29:39 +0200

    iniciar sesión actualizado y intento de switch

--- main.c
+++ main.c
@@ -32,6 +32,7 @@ void clearIfNeeded(char *str, int max_line){
 		while (getchar() != '\n');
 }
 
+
 char menu(){
 		printf("\n1. Iniciar sesion como administrador\n");
 		printf("2. Iniciar sesion como socio\n");
@@ -46,7 +47,9 @@ char menu(){
 		clearIfNeeded(linea, MAX_LINE);
 		return *linea;
 }
-char registrarSocio(){
+
+
+char registrarSocio(){ //cambiar
 	printf("\n1. Iniciar sesion como administrador\n");
 	printf("2. Iniciar sesion como socio\n");
 	printf("3. Iniciar sesion como trabajador\n");
@@ -60,6 +63,8 @@ char registrarSocio(){
 	clearIfNeeded(linea, MAX_LINE);
 	return *linea;
 }
+
+
 char menuAdministrador(){ ///cambiar por los valores que queramos introducir
 		printf("\n1. Crear socios\n");
 		printf("2. Crear trabajadores\n");
@@ -129,16 +134,18 @@ char menuTrabajador(){ ///cambiar por los valores que queramos introducir
 	printf("5.Ver lista de torneos/actividades\n");
 	printf("6. Salir\n");
 	printf("\n");
-	printf("Opcion? ->  ");
+	printf("Opcion ->  ");
+
 	fflush(stdout);
 	char linea[MAX_LINE];
 	fgets(linea, MAX_LINE, stdin);
 	clearIfNeeded(linea, MAX_LINE);
-	return *linea;
+	//return *linea;
 
-	char opcionTrabajador;
-	opcionTrabajador= menuTrabajador();
-	switch (opcionTrabajador){
+	//char opcionTrabajador;
+	//opcionTrabajador= menuTrabajador();
+
+	/*switch (opcionTrabajador){
 		case '1':
 
 				break;
@@ -157,153 +164,96 @@ char menuTrabajador(){ ///cambiar por los valores que queramos introducir
 		case'6':
 				menu();
 				break;
-	}
+	}*/
+
+
 
 }
+
 void logIn(){
 	char usuario[Longitud];
 	char clave[Longitud];
 	int intento= 0;
-	int ingresa= 0;
+	int ingresaAdministrador= 0;
+	int ingresaSocio=0;
+	int ingresaTrabajador=0;
 	int i;
 	char caracter;
 
-	do {
-		i=0;
-		system("cls");
-		printf("\n\t\t\t INICIAR SESION EN EL POLIDEPORTIVO\n");
-		printf("\t\t\t **********************************\n");
-		printf("\n\t USUARIO: ");
-		fflush(stdout);
+		do {
+			i=0;
+			system("cls");
+			printf("\n\t\t\t INICIAR SESION EN EL POLIDEPORTIVO\n");
+			printf("\t\t\t **********************************\n");
+			printf("\n\t USUARIO: ");
+			fflush(stdout);
 
-		fgets(usuario, Longitud, stdin);
-		clearIfNeeded(usuario, Longitud);
-		usuario[strcspn(usuario, "\r\n")] = 0;
+			fgets(usuario, Longitud, stdin);
+			clearIfNeeded(usuario, Longitud);
+			usuario[strcspn(usuario, "\r\n")] = 0;
 
-		printf("\t CONTRASEÑA: ");
-		fflush(stdout);
-		fgets(clave, Longitud, stdin);
+			printf("\t CONTRASEÑA: ");
+			fflush(stdout);
+			//fgets(clave, Longitud, stdin);
 
-		/*while (caracter=getch()){					Esta función sirve para poner la contraseña oculta
-			if (caracter==13){
-				clave[i]= '\0';
-				break;
-			}else if(caracter==8){
-				if(i>0){
-					i--;
-					printf("\b \b");
-				}
-			}else{
-				if(i<Longitud){
-					printf("*");
-					clave[i]= caracter;
-					i++;
+			while (caracter=getch()){
+				if (caracter==13){
+					clave[i]= '\0';
+					break;
+				}else if(caracter==8){
+					if(i>0){
+						i--;
+						printf("\b \b");
+					}
+				}else{
+					if(i<Longitud){
+						printf("*");
+						clave[i]= caracter;
+						i++;
+					}
 				}
 			}
-		}*/
 
-		clearIfNeeded(clave, Longitud);
-		clave[strcspn(clave, "\r\n")] = 0;
-
-
-		if (strcmp(usuario,Usuario)==0 && strcmp(clave,Clave)==0) {
-			ingresa=1;
-
-		}else {
-			printf("\n\t Usuario y/o clave son incorrectos\n");
-			intento++;
-			getchar();
-
-		}
-	} while (intento<3 && ingresa==0);
-
-	if (ingresa==1) {
-		printf("\n\t ¡Bienvenido al sistema!\n");
-		menuAdministrador();// aqui se añade el menu de administrador una vez puesta la contraseña correcta
-
-	}else{
-		printf("\n\t Has sobrepasado el numero maximo de intentos\n");
-	}
-}
-void socio(){
-	char email[Longitud];
-	char contrasenya[Longitud];
-	int intento= 0;
-	int ingresa= 0;
-
-	do {
-		system("cls");
-		printf("\n\t\t\t INICIA SESION COMO SOCIO\n");
-		printf("\n\t Email: ");
-		fflush(stdout);
-
-		fgets(email, Longitud, stdin);
-		clearIfNeeded(email, Longitud);
-		email[strcspn(email, "\r\n")] = 0;
-
-		printf("\t Contraseña: ");
-		fflush(stdout);
-		fgets(contrasenya, Longitud, stdin);
-		clearIfNeeded(contrasenya, Longitud);
-		contrasenya[strcspn(contrasenya, "\r\n")] = 0;
+			clearIfNeeded(clave, Longitud);
+			clave[strcspn(clave, "\r\n")] = 0;
 
 
-		if (strcmp(email,Email)==0 && strcmp(contrasenya,Contrasenya)==0) {
-			ingresa=1;
+			if (strcmp(usuario,Usuario)==0 && strcmp(clave,Clave)==0) {
+				ingresaAdministrador=1;
 
-		}else {
-			printf("\n\t Email y/o Contraseña son incorrectos\n");
-			intento++;
-			getchar();
 
-		}
-	} while (intento<3 && ingresa==0);
+	        }else if(strcmp(usuario,Email)==0 && strcmp(clave,Contrasenya)==0){
+	            ingresaSocio=1;
 
-	if (ingresa==1) {
-		printf("\n\t Bienvenido al sistema\n");
-		menuSocio();//Ir a la ventana correspondiente
-	}else{
-		printf("\n\t Has sobrepasado el numero maximo de intentos\n");
-	}
-}
-void trabajador(){
-	char dni[Longitud];
-	char contrasenya[Longitud];
-	int intento= 0;
-	int ingresa= 0;
+	        }else if(strcmp(usuario,DNI)==0 && strcmp(clave,Contrasenya)==0){
+	            ingresaTrabajador=1;
 
-	do {
-		system("cls");
-		printf("\n\t\t\t INICIA SESION COMO TRABAJADOR\n");
-		printf("\n\t DNI: ");
-		fflush(stdout);
+	        }else {
+				printf("\n\t Usuario y/o clave son incorrectos\n");
+				intento++;
+				getchar();
 
-		fgets(dni, Longitud, stdin);
-		clearIfNeeded(dni, Longitud);
-		dni[strcspn(dni, "\r\n")] = 0;
-
-		printf("\t Contraseña: ");
-		fflush(stdout);
-		fgets(contrasenya, Longitud, stdin);
-		clearIfNeeded(contrasenya, Longitud);
-		contrasenya[strcspn(contrasenya, "\r\n")] = 0;
-
-		if (strcmp(dni,DNI)==0 && strcmp(contrasenya,Contrasenya)==0) {
-			ingresa=1;
-		}else {
-			printf("\n\t Email y/o Contraseña son incorrectos\n");
-			intento++;
-			getchar();
 			}
-	} while (intento<3 && ingresa==0);
+		} while (intento<3 && ingresaAdministrador==0 && ingresaSocio==0 && ingresaTrabajador==0);
 
-	if (ingresa==1) {
-		printf("\n\t Bienvenido al sistema\n");
-		menuTrabajador();//Ir a la ventana correspondiente
-	}else{
-		printf("\n\t Has sobrepasado el numero maximo de intentos\n");
-	}
+		if (ingresaAdministrador==1) {
+			printf("\n\t ¡Bienvenido al sistema como administrador!\n");
+			menuAdministrador();// aqui se añade el menu de administrador una vez puesta la contraseña correcta
+
+	    }else if(ingresaSocio==1){
+	        printf("\n\t ¡Bienvenido al sistema como socio!\n");
+	        menuSocio();
+	    }else if(ingresaTrabajador==1){
+	        printf("\n\t ¡Bienvenido al sistema como trabajador!\n");
+	        menuTrabajador();
+
+
+		}else{
+			printf("\n\t Has sobrepasado el numero maximo de intentos\n");
+		}
 }
+
+
 
 
 int main(void){
@@ -315,11 +265,11 @@ int main(void){
 					break;
 
 			case '2':
-					socio();
+					logIn();
 					break;
 
 			case '3':
-					trabajador();
+					logIn();
 					break;
 
 			case '4':
